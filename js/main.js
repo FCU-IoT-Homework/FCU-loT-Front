85f9f86759b81a8f1ddb348326905f444b34a352
Kevin <ds934118@gmail.com>
2016-05-10 20:30:12 +0800

    First Test

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var app = angular.module("hackathon", ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/home/:id", {
+            templateUrl: "views/home.html",
+            controller: "homeCon"
+        }).when("/setting", {
+            templateUrl: "views/setting.html",
+            controller: "settingCon"
+        }).when("/about", {
+            templateUrl: "views/about.html"
+        }).when("/progress", {
+            templateUrl: "views/progress.html"
+        }).when("/remote", {
+            templateUrl: "views/remote.html"
+        }).otherwise({
+            redirectTo: "/"
+        });
+});
