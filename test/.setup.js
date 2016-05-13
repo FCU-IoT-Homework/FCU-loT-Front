e11f1642d522cf74d0d1b9f4f0a8046cf79fe584
Heng-xiu <r23456999@gmail.com>
2016-05-13 17:34:57 +0800

    測試環境建立完成 幹！！！！！

--- test/.setup.js
+++ test/.setup.js
@@ -1,12 +1,15 @@
 require('babel-register')();
+require('mdl-react');
 
 var jsdom = require('node-jsdom').jsdom;
+var chai = require('chai');
+global.expect = chai.expect;
 
 var exposedProperties = ['window', 'navigator', 'document'];
 
 global.document = jsdom('');
 global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
+Object.keys(document.defaultView).forEach(function(property){
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
     global[property] = document.defaultView[property];
--- test/generic-test.js
+++ test/generic-test.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { mount, shallow } from 'enzyme';
+var React = require('react');
+var enzyme = require('enzyme');
+var Generic = require('../example/views/generic.jsx');
 
-describe('<Foo />', () => {
+describe('<Generic />', function(){
 
-  it('calls componentDidMount', () => {
-    const wrapper = mount(<Foo />);
-    expect(Foo.prototype.componentDidMount.calledOnce).to.equal(true);
-  });
+	it('should render Title', function(){
+		var wrapper = enzyme.shallow(<Generic />);
+		// console.log(wrapper.debug());
+		expect(wrapper.find('DocTitle'));
+	});
+
+
+	it('should render ”一般偵測“ ', function(){
+		var wrapper = enzyme.shallow(<Generic />);
+		expect(wrapper.find('一般偵測'));
+	});
+
+	it('should render h3', function(){
+		var wrapper = enzyme.shallow(<Generic />);
+		expect(wrapper.find('h3').length).to.equal(4);
+	});
+
 
 });
