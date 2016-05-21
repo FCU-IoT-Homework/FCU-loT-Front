5868d4c4b103422d4f12f670b80a23748e95604b
Heng-xiu <r23456999@gmail.com>
2016-05-22 02:46:23 +0800

    改寫ＡＰＩ

--- app/app.js
+++ app/app.js
@@ -199,10 +199,27 @@ module.exports = {
 },{"./CardWithBtn.jsx":2,"./CardWithValue.jsx":3,"./DocTitle.jsx":4}],6:[function(require,module,exports){
 
 var request = require('superagent');
-var base = 'http://140.134.26.72/IoT/Hardware/API';
+var baseFCU = 'http://140.134.26.72/IoT/Hardware/API';
+var baseTS = 'https://api.thingspeak.com/channels/65175';
 module.exports = {
 
 	get : function(temp, callback){
+		request
+			.get(baseFCU + temp)
+			.accept('application/json')
+			.end(function(err, res){
+				// console.debug('apiGet: res', res);
+				// console.debug('apiGet: res.body', res.body);
+				// console.debug('apiGet: res.text', res.text);
+				if(err){
+					console.error('Error in Generic.jsx: ', err);
+					return err;
+				}
+				return callback(res.body, res.text);
+			});
+	},
+
+	getTS : function(temp, callback){
 		request
 			.get(base + temp)
 			.accept('application/json')
